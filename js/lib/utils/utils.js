f7eb6d6c31ff28d61bc776e158013e6683ca0245
lior <liorz1984@gmail.com>
2013-01-30 12:42:06 +0200

    Added support for different layouts that depends on the page type

--- js/lib/utils/utils.js
+++ js/lib/utils/utils.js
@@ -5,5 +5,15 @@ var utils = utils || {};
 		var w = $(window).width()*percent;
 		
 		return {height: h, width: w};
+	};
+	
+	utils.hexToRgb = function(hex) {
+	    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+	    return result ? {
+	        r: parseInt(result[1], 16),
+	        g: parseInt(result[2], 16),
+	        b: parseInt(result[3], 16)
+	    } : null;
 	}
+
 }());
--- js/views/QuestPagePropertiesView.js
+++ js/views/QuestPagePropertiesView.js
@@ -12,18 +12,27 @@ $(function() {
 		},
 		render:function() {
 			this.$el.empty();	
-			this.$el.html(this.template(this.model.toJSON));
+			var type_obj = app.Attributes[this.model.get('page_type')];
+			
+			var fill_color = utils.hexToRgb(type_obj.view.fill);
+			
+			this.$el.html(this.template(this.model.toJSON()));
 			var size = utils.precentToPixels(0.85);
 			this.$el.css({
 				display: 'inline',
 				width: size.width,
 				height: size.height,
+				'background-color': 'rgba(' + fill_color.r +',' + fill_color.g + ',' + fill_color.b + ',' +'0.8)'
+
 			});
 		    this.$el.css("top", Math.max(0, (($(window).height() - this.$el.outerHeight()) / 2) + 
                     $(window).scrollTop()) + "px");
 		    this.$el.css("left", Math.max(0, (($(window).width() - this.$el.outerWidth()) / 2) + 
                     $(window).scrollLeft()) + "px");
-			console.log(this.$el.html());
+		    
+		    this.$('img').attr('src',type_obj.view.avatar);
+		    console.log(this.$('img').attr('src'));
+		    this.$('#type_title').text(type_obj.view.type_title);
 		}
 	});
 }());
