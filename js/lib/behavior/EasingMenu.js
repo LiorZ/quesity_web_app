c471489bd63e4b3e36634570a4cf8a4dd0e719d9
lior <liorz1984@gmail.com>
2013-03-07 12:53:58 +0200

    Adding EasingMenu which is essential for the menu fix

---
+++ js/lib/behavior/EasingMenu.js
@@ -0,0 +1,48 @@
+(function( $ ){
+
+	  $.fn.make_menu = function(options) {
+		  var start_loc = options.start_loc;
+		  var start_method = options.start_method;
+		  var end_loc = options.end_loc;
+		  var end_method = options.end_method;
+		  var menu_fold_callback = options.menu_fold_callback;
+		  var timer_in = undefined;
+		  var time_out = undefined;
+			$(this).mouseenter(function(e) {
+				console.log(this);
+				var elem = this;
+				time_out = setTimeout(function() {$(elem).animate(start_loc, start_method ); }, 200);
+				if ( timer_in )
+					clearTimeout(timer_in);
+			});
+			var context = this;
+			$(this).mouseleave(function(e) {
+				if (time_out){
+					clearTimeout(time_out);
+					time_out = undefined;
+				}
+				
+				timer_in = setTimeout(function() {
+					menu_fold_callback();
+					$(context).animate(end_loc, end_method); timer_in = undefined; 
+				}, 800);
+			});
+	  };
+	})( jQuery );
+	
+
+(function() {
+	$(document).ready(function() {
+		var menu_container_start_left = $("#menu").css('left');
+		
+		$("#menu").make_menu({
+			start_loc: {left  : 0}, 
+			start_method: {duration: 1000, method: 'easeInSine'},
+			end_loc: {left:menu_container_start_left},
+			end_method:{duration: 1000, method: 'easeOutSine'},
+			menu_fold_callback: function() { $("#new_page_menu").css('display','none'); }
+		});
+		
+	});
+	
+}());
